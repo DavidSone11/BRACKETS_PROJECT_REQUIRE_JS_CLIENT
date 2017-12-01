94a2eeddd8d9a9cb9c46ebe9516dbc9144e2f818
santosh <santosh_citech@live.in>
2017-12-01 13:54:50 +0530

    index file added

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
-
-
 router.post("/api/v1/user",function(req,res,next){
-
-    console.log("DSADASD"+req);
-
-
+    console.log("DSADASD"+req.body.username);
+    console.log("DSADASD"+req.body.password);
+   return res.json({
+        "message" :"User has been saved Successfully!!!"
+    })
 })
-
-
-
 module.exports = router;
--- server.js
+++ server.js
@@ -7,52 +7,38 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var db = require('./database/db');
 var routes = require('./routes/index');
-
 var app = express();
-
 var raw_port = process.env.PORT;
-
 process.argv.forEach(function(val, index, array) {
     var port_i = val.search(/^port=/i);
     if (port_i > -1) {
         raw_port = val.substring(port_i + 5, val.length);
-        
     }
 });
-
-
-
 function normalizePort(val) {
   var port = parseInt(val, 10);
-
   if (isNaN(port)) {
     // named pipe
     return val;
   }
-
   if (port >= 0) {
     // port number
     return port;
   }
-
   return false;
 }
-
 var port = normalizePort(raw_port || '4000');
 app.set('port', port);
-
 var server=app.listen(port, function () {
   console.log('Server listening on url: http://localhost:'+port);
 });
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'src')));
-
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -64,21 +50,15 @@ app.all('/*', function(req, res, next) {
     next();
   }
 });
-
-
 //app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
-
 app.use('/', routes);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -90,7 +70,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -100,9 +79,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 var from = {day:1,time:"01:00"};
 var to = {day:1,time:"00:59"};
-
 //console.log(timeCal.diffDateTimeObj(from,to));
 module.exports = app;
